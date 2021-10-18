2da2478e7cf037f717c2767cd344013a2fc9532c
Nguyen Thanh Tung <nttung997@gmail.com>
2021-10-18 16:09:29 +0700

    change books in homepage

--- src/store/modules/book.store.js
+++ src/store/modules/book.store.js
@@ -19,6 +19,19 @@ const actions = {
         })
     })
   },
+  new({ commit }) {
+    commit('success', { key: 'books', value: {} })
+    return new Promise((resolve, reject) => {
+      axios.get(`new`)
+        .then(response => {
+          commit('success', { key: 'books', value: response.data })
+          resolve(response)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  },
   books({ commit }, isbn13) {
     commit('success', { key: 'book', value: {} })
     return new Promise((resolve, reject) => {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@ export default {
     }),
   },
   created() {
-    this.$store.dispatch("book/search", "JavaScript");
+    this.$store.dispatch("book/new");
   },
 };
 </script>
